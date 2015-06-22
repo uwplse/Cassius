f0219a379af1adf83f016814259eabb22014e397
Pavel Panchekha <me@pavpanchekha.com>
2015-06-22 13:27:52 -0700

    Add (reap) to a common file

---
+++ cassius/common.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+(provide reap)
+
+(define-syntax-rule (reap [sows ...] body ...)
+  (let* ([sows (let ([store '()])
+		 (λ (elt) (if elt
+			      (begin (set! store (cons elt store))
+				     elt)
+			      store)))] ...)
+    body ...
+    (values (reverse (sows #f)) ...)))
