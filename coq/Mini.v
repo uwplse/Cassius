d9bf8e31b491cd7a1f05acbec8c2f39b8a35bc52
lolisa <lolisa@marisa.moe>
2015-09-16 16:48:09 -0700

    save a few extraction file

--- coq/Mini.v
+++ coq/Mini.v
@@ -74,4 +74,4 @@ Definition IfAndPreserve : forall e, BDenote (IfAnd e) = BDenote e.
   intros; eapply Forall_forall in H; eauto.
 Qed.
 Extraction Language Scheme.
-Recursive Extraction IfAnd.
+Extraction "z30.rkt" IfAnd.
