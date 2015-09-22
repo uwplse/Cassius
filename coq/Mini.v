e6bd6c71ed10079448ea35dea4af104c055df0dd
lolisa <lolisa@marisa.moe>
2015-09-22 09:08:32 -0700

    fix conflict

--- coq/Mini.v
+++ coq/Mini.v
@@ -7,14 +7,6 @@ Inductive expr :=
 | Or (l : list expr)
 | Imply (a b : expr)
 | Not (a : expr)
-<<<<<<< HEAD
-<<<<<<< HEAD
-| ETrue | EFalse
-=======
->>>>>>> 73a23e6... fix ffi
-=======
-| ETrue | EFalse
->>>>>>> f2ada7e... model a part of simpl1
 | Unknown (R : Rest) (l : list expr)
 | UnknownAtom (R : Rest).
 
@@ -33,16 +25,6 @@ Fixpoint BDenote (e : expr) : bool :=
   | Or l => existsb BDenote l
   | Imply a b => implb (BDenote a) (BDenote b)
   | Not a => negb (BDenote a)
-<<<<<<< HEAD
-<<<<<<< HEAD
-  | ETrue => true
-  | EFalse => false
-=======
->>>>>>> 73a23e6... fix ffi
-=======
-  | ETrue => true
-  | EFalse => false
->>>>>>> f2ada7e... model a part of simpl1
   | Unknown R l => UnknownDenote R l
   | UnknownAtom R => UnknownAtomDenote R
   end.
@@ -55,16 +37,6 @@ Fixpoint Size (e : expr) :=
     | Or l => (fold_left plus (map Size l) 0)
     | Imply a b => (Size a + Size b)
     | Not a => Size a
-<<<<<<< HEAD
-<<<<<<< HEAD
-    | ETrue => 0
-    | EFalse => 0
-=======
->>>>>>> 73a23e6... fix ffi
-=======
-    | ETrue => 0
-    | EFalse => 0
->>>>>>> f2ada7e... model a part of simpl1
     | Unknown R l => (fold_left plus (map Size l) 0)
     | UnknownAtom R => 0
     end.
@@ -96,16 +68,6 @@ Fixpoint IfAnd (ex : expr) : expr :=
   | Or l => Or (map IfAnd l)
   | Imply l r => Imply (IfAnd l) (IfAnd r)
   | Not a => Not (IfAnd a)
-<<<<<<< HEAD
-<<<<<<< HEAD
-  | ETrue => ETrue
-  | EFalse => EFalse
-=======
->>>>>>> 73a23e6... fix ffi
-=======
-  | ETrue => ETrue
-  | EFalse => EFalse
->>>>>>> f2ada7e... model a part of simpl1
   | Unknown r l => Unknown r (map IfAnd l)
   | UnknownAtom r => UnknownAtom r
   end.
@@ -120,39 +82,15 @@ Program Fixpoint expr_ind_Forall
   (POr : forall l, Forall P l -> P (Or l))
   (PImply : forall a b, P a -> P b -> P (Imply a b))
   (PNot : forall a, P a -> P (Not a))
-<<<<<<< HEAD
-<<<<<<< HEAD
-  (PETrue : P ETrue) (PEFalse : P EFalse)
-  (PUnknown : forall R l, Forall P l -> P (Unknown R l))
-  (PUnknownAtom : forall R, P (UnknownAtom R)) e : P e :=
-  let F := (expr_ind_Forall P PIf PAnd POr PImply PNot PETrue PEFalse PUnknown PUnknownAtom) in
-=======
   (PUnknown : forall R l, Forall P l -> P (Unknown R l))
   (PUnknownAtom : forall R, P (UnknownAtom R)) e : P e :=
   let F := (expr_ind_Forall P PIf PAnd POr PImply PNot PUnknown PUnknownAtom) in
->>>>>>> 73a23e6... fix ffi
-=======
-  (PETrue : P ETrue) (PEFalse : P EFalse)
-  (PUnknown : forall R l, Forall P l -> P (Unknown R l))
-  (PUnknownAtom : forall R, P (UnknownAtom R)) e : P e :=
-  let F := (expr_ind_Forall P PIf PAnd POr PImply PNot PETrue PEFalse PUnknown PUnknownAtom) in
->>>>>>> f2ada7e... model a part of simpl1
     match e with
     | If a b c => PIf a b c (F a) (F b) (F c)
     | And l => PAnd l (list_ind _ _ (fun ex _ _ => Forall_cons ex (F ex) _) l)
     | Or l => POr l (list_ind _ _ (fun ex _ _ => Forall_cons ex (F ex) _) l)
     | Imply a b => PImply a b (F a) (F b)
     | Not a => PNot a (F a)
-<<<<<<< HEAD
-<<<<<<< HEAD
-    | ETrue => PETrue
-    | EFalse => PEFalse
-=======
->>>>>>> 73a23e6... fix ffi
-=======
-    | ETrue => PETrue
-    | EFalse => PEFalse
->>>>>>> f2ada7e... model a part of simpl1
     | Unknown R l => PUnknown R l (list_ind _ _ (fun ex _ _ => Forall_cons ex (F ex) _) l)
     | UnknownAtom R => PUnknownAtom R
     end.
@@ -167,48 +105,4 @@ Definition IfAndPreserve : forall e, BDenote (IfAnd e) = BDenote e.
     try rewrite !IfAndRPreserve in *; try congruence).
   apply RestBDenoteModular; simpl; rewrite H2; f_equal.
   rewrite map_map; apply map_ext_in; intros; eapply Forall_forall in H3; eauto.
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f2ada7e... model a part of simpl1
-Qed.
-
-Definition Simpl1 e :=
-  match e with
-  | If ETrue a _ => a
-  | If EFalse _ a => a
-  | If a ETrue EFalse => a
-  | If a EFalse ETrue => Not a
-  | If _ a b => if exprEqDec a b then a else e
-  | Imply EFalse _ => ETrue
-  | Imply ETrue a => a
-  | Not (Not a) => a
-  | And [] => ETrue
-  | And l => 
-      if In_dec exprEqDec EFalse l 
-      then EFalse 
-      else
-        let l := (remove exprEqDec ETrue l) in 
-          match l with [] => ETrue | _ => And l end
-  | Or [] => EFalse
-  | Or l => 
-      if In_dec exprEqDec ETrue l 
-      then ETrue 
-      else 
-        let l := (remove exprEqDec EFalse l) in 
-          match l with [] => EFalse | _ => Or l end
-  | _ => e
-  end.
-
-Theorem Simpl1Preserve e : BDenote (Simpl1 e) = BDenote e.
-  destruct e; simpl; try induction l;
-  repeat (
-    unfold negb in *; unfold andb in *; unfold orb in *;
-    try match goal with H : _ :: _ = _ :: _ |- _ => invcs H end;
-    try match_destruct; subst; simpl in *; try congruence).
-<<<<<<< HEAD
-=======
->>>>>>> 73a23e6... fix ffi
-=======
->>>>>>> f2ada7e... model a part of simpl1
 Qed.
