ba4b26e527adb4eef14684c3aaed2adb706de4b0
Pavel Panchekha <me@pavpanchekha.com>
2015-09-22 17:01:42 -0700

    Improved Makefile

--- Makefile
+++ Makefile
@@ -4,10 +4,18 @@ TESTS=$(TEST_FILES:bench/%.rkt=%:verify)
 .PHONY: download deploy
 
 test:
-	bash test.sh $(TESTS)
+	bash test.sh -j4 $(TESTS)
 
 deploy:
 	rsync -r www/ $(shell ~/uwplse/getdir)
 
 download:
 	grep ';; python get_bench.py' -R bench/ | cut '-d:' -f2- | cut -c4- | xargs -n1 bash -c
+
+bench/css/%.rkt: Makefile get_bench.py get_bench.js
+	python get_bench.py --name css/$* $(patsubst %,file://%,$(wildcard /home/pavpan/src/csswg-test/css21/$*/*.xht))
+
+get-csswg: bench/css/floats.rkt bench/css/margin-padding-clear.rkt
+
+test-csswg-%: #bench/css/%.rkt
+	bash test.sh -j8 $(shell seq -f css/$*:doc-%g 76 500)
--- test.sh
+++ test.sh
@@ -1,37 +1,57 @@
 #!/bin/sh
 set -e
 
-IFILE=`mktemp /tmp/tmp.XXXXXXXXXX`
-OFILE=`mktemp /tmp/tmp.XXXXXXXXXX`
-
-START=`date +%s%N | cut -b1-13`
+MAX=0
 
 run() {
-    if racket cassius/run.rkt bench/$fname.rkt $pname >/dev/null 2>/tmp/cassius.test; then
-        NOW=`date +%s%N | cut -b1-13`
-        echo $fname $pname `tput setaf 2`✔`tput sgr0` "["$((NOW-START))ms"]"
-        echo $fname $pname 1 >> $OFILE
+    local START=`date +%s%N | cut -b1-13`
+    if nice racket cassius/run.rkt bench/$fname.rkt $pname >/dev/null 2>/tmp/cassius.test; then
+        local NOW=`date +%s%N | cut -b1-13`
+        echo $fname $pname `tput setaf 2`✔`tput sgr0` "["$((NOW-START))ms"]" 1>&2
+        echo $fname $pname 1
     else
-        NOW=`date +%s%N | cut -b1-13`
-        echo $fname $pname `tput setaf 1`✘`tput sgr0` "["$((NOW-START))ms"]"
-        echo $fname $pname 0 >> $OFILE
+        local NOW=`date +%s%N | cut -b1-13`
+        echo $fname $pname `tput setaf 1`✘`tput sgr0` "["$((NOW-START))ms"]" 1>&2
+        echo $fname $pname 0
     fi
 }
 
-for arg in "$@"; do
-    out=`echo $arg | tr : \ `
-    fname=$(echo $out | cut -d\  -f1)
-    pname=$(echo $out | cut -d\  -f2)
-    echo $fname $pname >> $IFILE
-    run $fname $pname &
+wait() {
+    GOING=1
+    while [[ $GOING = 1 ]]; do
+        sleep .2
+        A=`wc -l <$OFILE`
+        B=`wc -l <$IFILE`
+        if [[ $((B-A)) -lt $MAX ]]; then GOING=0; fi
+    done
+}
+
+while getopts "j:" flag; do
+    case "${flag}" in
+        j) MAX="${OPTARG}"; shift ;;
+        *) break ;;
+    esac
 done
 
-GOING=1
-while [[ $GOING = 1 ]]; do
-    sleep .2
-    A=`wc -l <$OFILE`
-    B=`wc -l <$IFILE`
-    if [[ $A = $B ]]; then GOING=0; fi
-done
 
-rm $OFILE
+if [[ $MAX = 0 ]]; then
+    for arg in "$@"; do
+        out=`echo $arg | tr : \ `
+        fname=$(echo $out | cut -d\  -f1)
+        pname=$(echo $out | cut -d\  -f2)
+        run $fname $pname >/dev/null
+    done
+else
+    IFILE=`mktemp /tmp/tmp.XXXXXXXXXX`
+    OFILE=`mktemp /tmp/tmp.XXXXXXXXXX`
+    for arg in "$@"; do
+        out=`echo $arg | tr : \ `
+        fname=$(echo $out | cut -d\  -f1)
+        pname=$(echo $out | cut -d\  -f2)
+        echo $fname $pname >> $IFILE
+        run $fname $pname >> $OFILE &
+        wait
+    done
+    wait
+    rm $OFILE
+fi
