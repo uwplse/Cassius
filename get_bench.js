4e0409d1eeb2e90e6e6e141601d89be7265b8a14
Pavel Panchekha <me@pavpanchekha.com>
2016-05-31 17:16:44 -0700

    Fix width bugs

--- get_bench.js
+++ get_bench.js
@@ -2,7 +2,7 @@
 javascript:void((function(x){x.src = "http://localhost:8000/get_bench.js"; document.querySelector("head").appendChild(x)})(document.createElement("script")));
 */
 
-Props = "width height margin-top margin-right margin-bottom margin-left padding-top padding-right padding-bottom padding-left border-top-width border-right-width border-bottom-width border-left-width float display text-align border-top-style border-right-style border-bottom-style border-left-style overflow-x overflow-y position top bottom left right box-sizing min-width".split(" ");
+Props = "width height margin-top margin-right margin-bottom margin-left padding-top padding-right padding-bottom padding-left border-top-width border-right-width border-bottom-width border-left-width float display text-align border-top-style border-right-style border-bottom-style border-left-style overflow-x overflow-y position top bottom left right box-sizing min-width max-width min-height max-height".split(" ");
 BadProps = "clear float direction min-height max-height max-width min-width overflow-x overflow-y position box-sizing white-space".split(" ");
 BadTags = "img input svg:svg".split(" ");
 
@@ -482,9 +482,6 @@ function dump_rule(sel, style, features, is_from_style) {
     var has_good_prop = false;
 
     var em2px = null;
-    if (style["max-width"]) {
-        style["width"] = style["max-width"];
-    }
 
     for (var i = 0; i < style.length; i++) {
         var sname = style[i];
