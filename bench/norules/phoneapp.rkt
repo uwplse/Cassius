d217f17ee68f7becb9d386863d6417fd9be6ec9d
Pavel Panchekha <me@pavpanchekha.com>
2016-06-28 14:33:07 -0400

    Better/simpler layout on mockup

--- bench/norules/phoneapp.rkt
+++ bench/norules/phoneapp.rkt
@@ -1,13 +1,14 @@
 (define-header header "
 * { padding: 0; margin: 0; }
-div.rect { outline: 1px solid black; }")
+div { outline: 1px solid black; }")
 
 (define-stylesheet main)
 
 (define-document (mockup #:width 750)
   ((BLOCK :tag html :w 750 :x 0 :y 0 :style ())
-   ((BLOCK :tag body :w 750 :h 1075 :x 0 :y 109.0
-           :style ([padding (px 25) (px 25) (px 25) (px 25)] [position absolute] [width (% 100)] [top (px 109)] [bottom (px 150)]))
+   ((BLOCK :tag header :x 0 :y 0 :w 750 :h 109 :style ([height ?])))
+   ((BLOCK :tag body :w 750 :x 0 :y 109.0
+           :style ([padding ? ? ? ?]))
     ((BLOCK :tag div :style ())
      ((BLOCK :tag form :w 500 :h 75 :x 125 :y 159.0
              :style ([margin ? ? ? ?] [padding ? ? ? ?] [width ?] [height ?]))
@@ -19,45 +20,45 @@ div.rect { outline: 1px solid black; }")
      ((BLOCK :tag rect :w 650 :h 75 :x 50 :y 259.0
              :style ([height ?] [margin ? ? ? ?])))
      ((BLOCK :tag g :style ())
-      ((BLOCK :tag rect :w 300 :h 200 :x 50 :y 359.0 
+      ((BLOCK :tag rect :w 300 :h 200 :x 50 :y 359.0
               :style ([float ?] [padding ? ? ? ?] [margin ? ? ? ?]))
-       ((BLOCK :tag rect :w 250 :h 150 :x 75 :y 384.0 
+       ((BLOCK :tag rect :w 250 :h 150 :x 75 :y 384.0
                :style ([height ?] [width ?]))))
-      ((BLOCK :tag rect :w 300 :h 200 :x 400 :y 359.0 
+      ((BLOCK :tag rect :w 300 :h 200 :x 400 :y 359.0
               :style ([float ?] [padding ? ? ? ?] [margin ? ? ? ?]))
-       ((BLOCK :tag rect :w 250 :h 150 :x 425 :y 384.0 
+       ((BLOCK :tag rect :w 250 :h 150 :x 425 :y 384.0
                :style ([height ?] [width ?]))))
-      ((BLOCK :tag rect :w 300 :h 200 :x 50 :y 609.0 
+      ((BLOCK :tag rect :w 300 :h 200 :x 50 :y 609.0
               :style ([float ?] [padding ? ? ? ?] [margin ? ? ? ?]))
-       ((BLOCK :tag rect :w 250 :h 150 :x 75 :y 634.0 
+       ((BLOCK :tag rect :w 250 :h 150 :x 75 :y 634.0
                :style ([height ?] [width ?]))))
-      ((BLOCK :tag rect :w 300 :h 200 :x 400 :y 609.0 
+      ((BLOCK :tag rect :w 300 :h 200 :x 400 :y 609.0
               :style ([float ?] [padding ? ? ? ?] [margin ? ? ? ?]))
-       ((BLOCK :tag rect :w 250 :h 150 :x 425 :y 634.0 
+       ((BLOCK :tag rect :w 250 :h 150 :x 425 :y 634.0
                :style ([height ?] [width ?]))))
-      ((BLOCK :tag rect :w 300 :h 200 :x 50 :y 859.0 
+      ((BLOCK :tag rect :w 300 :h 200 :x 50 :y 859.0
               :style ([float ?] [padding ? ? ? ?] [margin ? ? ? ?]))
-       ((BLOCK :tag rect :w 250 :h 150 :x 75 :y 884.0 
+       ((BLOCK :tag rect :w 250 :h 150 :x 75 :y 884.0
                :style ([height ?] [width ?]))))
-      ((BLOCK :tag rect :w 300 :h 200 :x 400 :y 859.0 
+      ((BLOCK :tag rect :w 300 :h 200 :x 400 :y 859.0
               :style ([float ?] [padding ? ? ? ?] [margin ? ? ? ?]))
-       ((BLOCK :tag rect :w 250 :h 150 :x 425 :y 884.0 
+       ((BLOCK :tag rect :w 250 :h 150 :x 425 :y 884.0
                :style ([height ?] [width ?])))))))
-   ((BLOCK :tag g 
+   ((BLOCK :tag g
            :style ([width (px 600)] [margin-left (px -300)] [position absolute] [bottom (px 0)] [left (% 50)] [height (px 150)]))
-    ((BLOCK :tag rect :w 100 :h 100 :x 100 :y 1209.0 
+    ((BLOCK :tag rect :w 100 :h 100 :x 100 :y 1209.0
             :style ([float ?] [height ?] [width ?] [margin ? ? ? ?])))
-    ((BLOCK :tag rect :w 100 :h 100 :x 250 :y 1209.0 
+    ((BLOCK :tag rect :w 100 :h 100 :x 250 :y 1209.0
             :style ([float ?] [height ?] [width ?] [margin ? ? ? ?])))
-    ((BLOCK :tag rect :w 100 :h 100 :x 400 :y 1209.0 
+    ((BLOCK :tag rect :w 100 :h 100 :x 400 :y 1209.0
             :style ([float ?] [height ?] [width ?] [margin ? ? ? ?])))
-    ((BLOCK :tag rect :w 100 :h 100 :x 550 :y 1209.0 
+    ((BLOCK :tag rect :w 100 :h 100 :x 550 :y 1209.0
             :style ([float ?] [height ?] [width ?] [margin ? ? ? ?]))))))
 
 (define-document (bigger #:width 1100)
   ((BLOCK :tag html :w 1100 :x 0 :y 0.0 :style ())
-   ((BLOCK :tag body :w 1100 :h 1661 :x 0 :y 109.0
-           :style ([padding ? ? ? ?] [position ?] [width ?] [top ?] [bottom ?]))
+   ((BLOCK :tag header :x 0 :y 0 :w 1100 :h 109 :style ([height ?])))
+   ((BLOCK :tag body :w 1100 :x 0 :y 109.0 :style ([padding ? ? ? ?]))
     ((BLOCK :tag div :style ())
      ((BLOCK :tag form :w 500 :h 75 :x 300 :y 159.0
              :style ([margin ? ? ? ?] [padding ? ? ? ?] [width ?] [height ?]))
@@ -115,58 +116,59 @@ div.rect { outline: 1px solid black; }")
   #:documents mockup bigger)
 
 (define-document (mockup-correct #:width 750)
-  ((BLOCK :tag html :w 750 :x 0 :y 0 
+  ((BLOCK :tag html :w 750 :x 0 :y 0
           :style ())
-   ((BLOCK :tag rect :w 750 :h 1075 :x 0 :y 109.0 
-           :style ([padding (px 25) (px 25) (px 25) (px 25)] [position absolute] [width (% 100)] [top (px 109)] [bottom (px 150)]))
-    ((BLOCK :tag g 
+   ((BLOCK :tag header :x 0 :y 0 :w 750 :h 109 :style ([height (px 109)])))
+   ((BLOCK :tag rect :w 750 :x 0 :y 109.0
+           :style ([padding (px 25) (px 25) (px 25) (px 25)]))
+    ((BLOCK :tag g
             :style ())
-     ((BLOCK :tag rect :w 500 :h 75 :x 125 :y 159.0 
+     ((BLOCK :tag rect :w 500 :h 75 :x 125 :y 159.0
              :style ([margin (px 25) auto (px 25) auto] [padding (px 10) (px 10) (px 10) (px 10)] [width (px 480)] [height (px 55)]))
-      ((BLOCK :tag g 
+      ((BLOCK :tag g
               :style ())
-       ((BLOCK :tag rect :w 215 :h 55 :x 135 :y 169.0 
+       ((BLOCK :tag rect :w 215 :h 55 :x 135 :y 169.0
                :style ([height (px 55)] [width (px 215)] [float left] [margin-right (px 50)])))
-       ((BLOCK :tag rect :w 215 :h 55 :x 400 :y 169.0 
+       ((BLOCK :tag rect :w 215 :h 55 :x 400 :y 169.0
                :style ([height (px 55)] [width (px 215)] [float left])))))
-     ((BLOCK :tag rect :w 650 :h 75 :x 50 :y 259.0 
+     ((BLOCK :tag rect :w 650 :h 75 :x 50 :y 259.0
              :style ([height (px 75)] [margin (px 0) (px 25) (px 0) (px 25)])))
-     ((BLOCK :tag g 
+     ((BLOCK :tag g
              :style ())
-      ((BLOCK :tag rect :w 300 :h 200 :x 50 :y 359.0 
+      ((BLOCK :tag rect :w 300 :h 200 :x 50 :y 359.0
               :style ([float left] [padding (px 25) (px 25) (px 25) (px 25)] [margin (px 25) (px 25) (px 25) (px 25)]))
-       ((BLOCK :tag rect :w 250 :h 150 :x 75 :y 384.0 
+       ((BLOCK :tag rect :w 250 :h 150 :x 75 :y 384.0
                :style ([height (px 150)] [width (px 250)]))))
-      ((BLOCK :tag rect :w 300 :h 200 :x 400 :y 359.0 
+      ((BLOCK :tag rect :w 300 :h 200 :x 400 :y 359.0
               :style ([float left] [padding (px 25) (px 25) (px 25) (px 25)] [margin (px 25) (px 25) (px 25) (px 25)]))
-       ((BLOCK :tag rect :w 250 :h 150 :x 425 :y 384.0 
+       ((BLOCK :tag rect :w 250 :h 150 :x 425 :y 384.0
                :style ([height (px 150)] [width (px 250)]))))
-      ((BLOCK :tag rect :w 300 :h 200 :x 50 :y 609.0 
+      ((BLOCK :tag rect :w 300 :h 200 :x 50 :y 609.0
               :style ([float left] [padding (px 25) (px 25) (px 25) (px 25)] [margin (px 25) (px 25) (px 25) (px 25)]))
-       ((BLOCK :tag rect :w 250 :h 150 :x 75 :y 634.0 
+       ((BLOCK :tag rect :w 250 :h 150 :x 75 :y 634.0
                :style ([height (px 150)] [width (px 250)]))))
-      ((BLOCK :tag rect :w 300 :h 200 :x 400 :y 609.0 
+      ((BLOCK :tag rect :w 300 :h 200 :x 400 :y 609.0
               :style ([float left] [padding (px 25) (px 25) (px 25) (px 25)] [margin (px 25) (px 25) (px 25) (px 25)]))
-       ((BLOCK :tag rect :w 250 :h 150 :x 425 :y 634.0 
+       ((BLOCK :tag rect :w 250 :h 150 :x 425 :y 634.0
                :style ([height (px 150)] [width (px 250)]))))
-      ((BLOCK :tag rect :w 300 :h 200 :x 50 :y 859.0 
+      ((BLOCK :tag rect :w 300 :h 200 :x 50 :y 859.0
               :style ([float left] [padding (px 25) (px 25) (px 25) (px 25)] [margin (px 25) (px 25) (px 25) (px 25)]))
-       ((BLOCK :tag rect :w 250 :h 150 :x 75 :y 884.0 
+       ((BLOCK :tag rect :w 250 :h 150 :x 75 :y 884.0
                :style ([height (px 150)] [width (px 250)]))))
-      ((BLOCK :tag rect :w 300 :h 200 :x 400 :y 859.0 
+      ((BLOCK :tag rect :w 300 :h 200 :x 400 :y 859.0
               :style ([float left] [padding (px 25) (px 25) (px 25) (px 25)] [margin (px 25) (px 25) (px 25) (px 25)]))
-       ((BLOCK :tag rect :w 250 :h 150 :x 425 :y 884.0 
+       ((BLOCK :tag rect :w 250 :h 150 :x 425 :y 884.0
                :style ([height (px 150)] [width (px 250)])))))))
-   ((BLOCK :tag g 
+   ((BLOCK :tag g
            :style ([width (px 600)] [position absolute] [bottom (px 0)] [left (% 50)] [margin-left (px -300)] [height (px 150)]))
-    ((BLOCK :tag rect :w 100 :h 100 :x 100 :y 1209.0 
-            :style ([float left] [height (px 100)] [width (px 100)] [margin (px 50) (px 25) (px 50) (px 25)])))
-    ((BLOCK :tag rect :w 100 :h 100 :x 250 :y 1209.0 
-            :style ([float left] [height (px 100)] [width (px 100)] [margin (px 50) (px 25) (px 50) (px 25)])))
-    ((BLOCK :tag rect :w 100 :h 100 :x 400 :y 1209.0 
-            :style ([float left] [height (px 100)] [width (px 100)] [margin (px 50) (px 25) (px 50) (px 25)])))
-    ((BLOCK :tag rect :w 100 :h 100 :x 550 :y 1209.0 
-            :style ([float left] [height (px 100)] [width (px 100)] [margin (px 50) (px 25) (px 50) (px 25)]))))))
+    ((BLOCK :tag rect :w 100 :h 100 :x 100 :y 1209.0
+            :style ([float left] [height (px 100)] [width (px 100)] [margin (px 25) (px 25) (px 25) (px 25)])))
+    ((BLOCK :tag rect :w 100 :h 100 :x 250 :y 1209.0
+            :style ([float left] [height (px 100)] [width (px 100)] [margin (px 25) (px 25) (px 25) (px 25)])))
+    ((BLOCK :tag rect :w 100 :h 100 :x 400 :y 1209.0
+            :style ([float left] [height (px 100)] [width (px 100)] [margin (px 25) (px 25) (px 25) (px 25)])))
+    ((BLOCK :tag rect :w 100 :h 100 :x 550 :y 1209.0
+            :style ([float left] [height (px 100)] [width (px 100)] [margin (px 25) (px 25) (px 25) (px 25)]))))))
 
 (define-problem one-done
   #:header header
