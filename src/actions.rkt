51877bfaf080ed9d108f0f5b674f921711e22259
Pavel Panchekha <me@pavpanchekha.com>
2016-08-24 12:34:18 -0400

    Fix script runner

--- src/actions.rkt
+++ src/actions.rkt
@@ -1,12 +1,13 @@
 #lang racket
 
-(require "selectors.rkt" "dom.rkt")
+(require "selectors.rkt" "tree.rkt")
+(provide interpret-action)
 
-(define (interpret-action act handlers dom)
+(define (interpret-action act handlers tree)
   (define-values (type target vals)
     (match act
       [(list 'click sel)
-       (define elts (for/list ([elt (in-elements dom)] #:when (selector-matches? sel elt)) elt))
+       (define elts (for/list ([elt (in-tree tree)] #:when (selector-matches? sel elt)) elt))
        (when (not (= (length elts) 1))
          (error "Invalid click action on more than one element" elts))
        (values 'click (car elts) (list (car elts)))]))
@@ -15,27 +16,28 @@
                 #:when (equal? (second handler) type)
                 #:when (selector-matches? (first handler) target))
       handler))
-  (match-define (list sel evt vars actions ...) handler)
-  (define env (map cons vars vals))
-  (for ([line actions])
-    (interpret-line dom line env)))
+  (when handler
+    (match-define (list sel evt vars actions ...) handler)
+    (define env (map cons vars vals))
+    (for ([line actions])
+      (interpret-line tree line env))))
 
-(define (interpret-line dom script env)
+(define (interpret-line tree script env)
   (match script
     [(list 'add-class eltexpr cls)
-     (define elts (interpret-eltexpr dom eltexpr env))
+     (define elts (interpret-eltexpr tree eltexpr env))
      (for ([elt elts])
-       (node-set! elt ':class (remove-duplicates (cons cls (node-get elt ':class)))))]
+       (node-set! elt ':class (remove-duplicates (append (node-get elt ':class) (list cls)))))]
     [(list 'remove-class eltexpr cls)
-     (define elts (interpret-eltexpr dom eltexpr env))
+     (define elts (interpret-eltexpr tree eltexpr env))
      (for ([elt elts])
        (node-set! elt ':class (filter (λ (x) (not (equal? x cls))) (node-get elt ':class))))]))
 
-(define (interpret-eltexpr dom eltexpr env)
+(define (interpret-eltexpr tree eltexpr env)
   (match eltexpr
     [(list 'select sel)
-     (for/list ([elt (in-elements dom)] #:when (selector-matches? sel elt)) elt)]
+     (for/list ([elt (in-tree tree)] #:when (selector-matches? sel elt)) elt)]
     [(list 'parent expr)
-     (map node-parent (interpret-eltexpr dom expr env))]
+     (map node-parent (interpret-eltexpr tree expr env))]
     [(? symbol?)
      (list (dict-ref env eltexpr))]))
