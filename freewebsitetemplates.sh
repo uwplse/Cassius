2b34bed37111033304dffe0a3bc1102752637e94
Pavel Panchekha <me@pavpanchekha.com>
2017-01-27 10:39:31 -0800

    Fix FWT script to hide browser window

--- freewebsitetemplates.sh
+++ freewebsitetemplates.sh
@@ -7,5 +7,5 @@ tmpdir=$(mktemp -d /tmp/cassius-get-bench.XXXXXX.zip)
 curl -L -s https://freewebsitetemplates.com/download/$name/ > $tmpdir/zip.zip
 unzip -q $tmpdir/zip.zip -d $tmpdir
 loc=$(find "$tmpdir" -name 'index.html' | head -n1)
-python get_bench.py "file://$loc" --name freewebsitetemplates/"$name" "$@"
+xvfb-run -s '-screen 0 1920x10800x24' python get_bench.py "file://$loc" --name freewebsitetemplates/"$name" "$@"
 rm -r $tmpdir
