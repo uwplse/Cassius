b3d80e4dfd2a4a00c98e531eaa6aa92fca22f8f6
Pavel Panchekha <me@pavpanchekha.com>
2017-04-27 16:52:11 -0700

    Don't redownload FWT websites every time

--- bench/fwt/get.sh
+++ bench/fwt/get.sh
@@ -3,9 +3,18 @@
 NAME=$1
 shift
 
+if [ ! -f bench/fwt/"$NAME".zip ]; then
+    curl -L -s https://freewebsitetemplates.com/download/"$NAME"/ > bench/fwt/"$NAME".zip
+fi
+
+if [ ! -s bench/fwt/"$NAME".zip ]; then
+    printf "ERROR! Downloaded empty file in %s\n" bench/fwt/"$NAME".zip
+    rm bench/fwt/"$NAME".zip
+    exit 1
+fi
+
 tmpdir=$(mktemp -d /tmp/cassius-get-bench.XXXXXX.zip)
-curl -L -s https://freewebsitetemplates.com/download/"$NAME"/ > $tmpdir/zip.zip
-unzip -q $tmpdir/zip.zip -d $tmpdir
+unzip -q bench/fwt/"$NAME".zip -d $tmpdir
 loc=$(find "$tmpdir" -name 'index.html' | head -n1)
 xvfb-run -s '-screen 0 1920x10800x24' python2 get_bench.py "file://$loc" --name fwt/"$NAME" "$@"
 rm -r $tmpdir
