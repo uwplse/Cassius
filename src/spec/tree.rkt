68f9a047e241cba4756c5f35e2e8e7896da0b158
Adam Geller <adam_geller@msn.com>
2017-09-20 17:01:54 +0200

    Fixed a mistake I made merging master into font-metrics

--- src/spec/tree.rkt
+++ src/spec/tree.rkt
@@ -8,60 +8,6 @@
 ;; encoding of CSS. It defines the Element and Box classes, and
 ;; functions to properly establish the pointers each holds.
 
-(define-constraints tree-types
-  (declare-datatypes ()
-     ((Box no-box
-           (box (type BoxType)
-                (x Real) (y Real) (w Real) (h Real) ; X, Y and width/height
-                (xo Real) (yo Real) ; X and Y offset
-                (mt Real) (mr Real) (mb Real) (ml Real) ; margins
-                (mtp Real) (mtn Real) (mbp Real) (mbn Real) ; top/bottom positive/negative margins for collapsing
-                (pt Real) (pr Real) (pb Real) (pl Real) ; padding
-                (bt Real) (br Real) (bb Real) (bl Real) ; border
-                (stfwidth Real) (stfmax Real) (float-stfmax Real) (w-from-stfwidth Bool)
-                (&pbox Int) (&vbox Int) (&nbox Int) (&fbox Int) (&lbox Int) ; box tree pointers
-                (width-set Bool) ; used for dependency creation only
-                (font-size Real) (leading Real) (ascender-top RealOpt) (descender-bottom RealOpt)
-                (text-top Real) (text-bottom Real) ; TODO: how do we compute this? Can we compute this?
-                (clh Real) ; computed line height
-                (&nflow Int) (&vflow Int) ; flow tree pointers
-                (&ppflow Int) ; parent positioned pointers
-                (&pbflow Int)
-                (&root Int) ; Root box
-                (&anc-w-elt Int) ; pointer to nearest ancestor with an element
-                (ez.in EZone) (ez.out EZone) (ez.sufficient Bool)
-                (has-contents Bool) (lineheight Line-Height) (textalign Text-Align) ; to handle inheritance; TODO: handle better
-                (&elt Int) (first-box? Bool) (last-box? Bool)
-                ,@(for/list ([i (in-naturals)] [(name p) (in-dict (extra-pointers))])
-                    `(,(sformat "&~a" i) Int))
-                (fg-color Color) (bg-color Color) (ancestor-bg Color)))
-      (BoxType box/root box/text box/inline box/block box/line)
-      (Element no-elt
-           (elt (specified-style Style) (computed-style Style) ; see compute-style.rkt
-                (is-replaced Bool) (intrinsic-width Real) (intrinsic-height Real)
-                (&pelt Int) (&velt Int) (&nelt Int) (&felt Int) (&lelt Int) (fid Int)))))
-
-  ,@(for/list ([field '(&pelt &velt &nelt &felt &lelt)])
-      `(assert (= (,field no-elt) -1)))
-  ,@(for/list ([field '(&pbox &vbox &nbox &fbox &lbox &nflow &vflow &ppflow &pbflow)])
-      `(assert (= (,field no-box) -1)))
-
-  (assert (= (&elt no-box) -1))
-
-  ;; TODO: for all three below functions, compute background color for transparent
-  ;; backgrounds. Currently stubbed out.
-  (define-fun fg-lum ((b Box)) Real
-    (lum (color.rgb (fg-color b))))
-
-  (define-fun bg-lum ((b Box)) Real
-    (lum (color.rgb (ancestor-bg b))))
-
-  ;; TODO: is this a good place to put this?
-  (define-fun color-distance ((c1 Color) (c2 Color)) Real
-    (ite (or (is-color/transparent c1) (is-color/transparent c2))
-         32.0
-         (color.diff (color.rgb c1) (color.rgb c2)))))
-
 (define-constraints link-definitions
   ;; The elements in each direction in the element tree
   (define-fun velt ((elt Element)) Element (get/elt (&velt elt)))
--- src/spec/utils.rkt
+++ src/spec/utils.rkt
@@ -27,6 +27,46 @@
   (define-fun between ((x Real) (y Real) (z Real)) Bool
     (or (<= x y z) (>= x y z))))
 
+(define-constraints tree-types
+  (declare-datatypes ()
+     ((Box no-box
+           (box (type BoxType)
+                (x Real) (y Real) (w Real) (h Real) ; X, Y and width/height
+                (xo Real) (yo Real) ; X and Y offset
+                (mt Real) (mr Real) (mb Real) (ml Real) ; margins
+                (mtp Real) (mtn Real) (mbp Real) (mbn Real) ; top/bottom positive/negative margins for collapsing
+                (pt Real) (pr Real) (pb Real) (pl Real) ; padding
+                (bt Real) (br Real) (bb Real) (bl Real) ; border
+                (stfwidth Real) (stfmax Real) (float-stfmax Real) (w-from-stfwidth Bool)
+                (&pbox Int) (&vbox Int) (&nbox Int) (&fbox Int) (&lbox Int) ; box tree pointers
+                (width-set Bool) ; used for dependency creation only
+                (font-size Real) (leading Real) (ascender-top RealOpt) (descender-bottom RealOpt)
+                (text-top Real) (text-bottom Real) ; TODO: how do we compute this? Can we compute this?
+                (clh Real) ; computed line height
+                (&nflow Int) (&vflow Int) ; flow tree pointers
+                (&ppflow Int) ; parent positioned pointers
+                (&pbflow Int)
+                (&root Int) ; Root box
+                (&anc-w-elt Int) ; pointer to nearest ancestor with an element
+                (ez.in EZone) (ez.out EZone) (ez.sufficient Bool)
+                (has-contents Bool) (lineheight Line-Height) (textalign Text-Align) ; to handle inheritance; TODO: handle better
+                (&elt Int) (first-box? Bool) (last-box? Bool)
+                ,@(for/list ([i (in-naturals)] [(name p) (in-dict (extra-pointers))])
+                    `(,(sformat "&~a" i) Int))
+                (fg-color Color) (bg-color Color) (ancestor-bg Color)))
+      (BoxType box/root box/text box/inline box/block box/line)
+      (Element no-elt
+           (elt (specified-style Style) (computed-style Style) ; see compute-style.rkt
+                (is-replaced Bool) (intrinsic-width Real) (intrinsic-height Real)
+                (&pelt Int) (&velt Int) (&nelt Int) (&felt Int) (&lelt Int) (fid Int)))))
+
+  ,@(for/list ([field '(&pelt &velt &nelt &felt &lelt)])
+      `(assert (= (,field no-elt) -1)))
+  ,@(for/list ([field '(&pbox &vbox &nbox &fbox &lbox &nflow &vflow &ppflow &pbflow)])
+      `(assert (= (,field no-box) -1)))
+
+  (assert (= (&elt no-box) -1)))
+
 (define-constraints utility-definitions
   (define-fun left-outer ((box Box)) Real (- (x box) (ml box)))
   (define-fun left-border ((box Box)) Real (x box))
