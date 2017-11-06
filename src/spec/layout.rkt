862f8e87d73bdb899af82102b25f471c45f5388e
Pavel Panchekha <me@pavpanchekha.com>
2017-11-06 11:58:17 -0800

    Fix how line height works

--- src/spec/layout.rkt
+++ src/spec/layout.rkt
@@ -515,26 +515,16 @@
   ;; Helper method for computing the line height and leading of a box
   (define-fun compute-line-height ((b Box)) Bool
     (and
-     (ite (is-line-height/normal (lineheight b))
-        (<= (max-if (* 1.1 (font-size b))
-                    (and (not (is-box/block (type b))) (is-box (pflow b)))
-                    (clh (pflow b)))
-            (clh b)
-            (max-if (* 1.25 (font-size b))
-                    (and (not (is-box/block (type b))) (is-box (pflow b)))
-                    (clh (pflow b))))
-        (= (clh b)
-           (max-if
-            ,(smt-cond
-              [(is-line-height/px (lineheight b))
-               (line-height.px (lineheight b))]
-              [(is-line-height/num (lineheight b))
-               (%of (* 100.0 (line-height.num (lineheight b))) (font-size b))]
-              [(is-line-height/% (lineheight b))
-               (%of (line-height.% (lineheight b)) (font-size b))]
-              [else 0]) ; Can't happen
-            (is-box (pflow b))
-            (clh (pflow b)))))
+     ,(smt-cond
+       [(is-line-height/normal (lineheight b))
+        (<= (* 1.1 (font-size b)) (clh b) (* 1.25 (font-size b)))]
+       [(is-line-height/num (lineheight b))
+        (= (clh b) (%of (* 100.0 (line-height.num (lineheight b))) (font-size b)))]
+       [(is-line-height/px (lineheight b))
+        (= (clh b) (line-height.px (lineheight b)))]
+       [(is-line-height/% (lineheight b))
+        (= (clh b) (%of (line-height.% (lineheight b)) (font-size b)))]
+       [else 0])
      (= (leading b) (- (clh b) (font-size b)))))
 
   ;; ez.line is a specialized thing for floats inside lines
