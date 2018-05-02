9dff8d7e6211c7e8f3409db71d5d2d05d5b4942f
Pavel Panchekha <me@pavpanchekha.com>
2018-05-02 16:48:27 -0700

    Added script to capture Joel blog post

---
+++ bench/joel/joel.js
@@ -0,0 +1,9 @@
+document.querySelector("#masthead").dataset["cassius"] = "magic";
+document.querySelector("#masthead .toggle-bar").dataset["cassius"] = "magic";
+document.querySelector("#panel-1 i").dataset["cassius"] = "magic";
+document.querySelector("#panel-1 span").dataset["cassius"] = "magic";
+document.querySelector("#panel-3 i").dataset["cassius"] = "magic";
+document.querySelector("#panel-3 i + i").dataset["cassius"] = "magic";
+document.querySelector("#panel-3 span").dataset["cassius"] = "magic";
+document.querySelector("h2.site-description").dataset["cassius"] = "magic";
+document.querySelector(".nav-links .meta-nav").childNodes[0].remove();
