2522c7ec4f2309bcccf08256bff66489fde4b31e
Pavel Panchekha <me@pavpanchekha.com>
2018-05-10 10:44:05 -0700

    Avoid stupid problem with the selection height?

--- bench/joel/joel.js
+++ bench/joel/joel.js
@@ -6,4 +6,5 @@ document.querySelector("#panel-3 i").dataset["cassius"] = "magic";
 document.querySelector("#panel-3 i + i").dataset["cassius"] = "magic";
 document.querySelector("#panel-3 span").dataset["cassius"] = "magic";
 document.querySelector("h2.site-description").dataset["cassius"] = "magic";
-document.querySelector(".nav-links .meta-nav").childNodes[0].remove();
+document.querySelectorAll(".nav-links .meta-nav")[0].innerText = "Previous Post";
+document.querySelectorAll(".nav-links .meta-nav")[1].innerText = "Next Post";
