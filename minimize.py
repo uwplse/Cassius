93928a583d6b280c2aa30f61975a31e618a7b2b7
Pavel Panchekha <me@pavpanchekha.com>
2018-06-01 14:12:09 -0700

    Simplify run_accept

--- minimize.py
+++ minimize.py
@@ -5,45 +5,44 @@ import argparse
 import subprocess
 import json
 import time
+import collections
 
 from get_bench import main as get_bench
 
-STATISTICS = []
+Done = collections.namedtuple("Done", ["size"])
+Backtrack = collections.namedtuple("Backtrack", [])
+Continue = collection.namedtuple("Continue", ["stats", "runtime", "elts"])
 
 def run_accept(name, cache_name, backtracked, maxtime=600):
+    start = time.time()
     try:
-        print("Running Cassius:", file=sys.stderr)
         cassius = subprocess.run(["racket", "src/run.rkt", "minimize", "--cache", cache_name, "bench/"+name+".rkt", "doc-1", "[{}]".format(",".join(backtracked))], stdout=subprocess.PIPE, timeout=maxtime)
     except subprocess.TimeoutExpired:
-            return (True, False, [], -1)
+            return Backtrack()
+    runtime = time.time() - start
 
     result = cassius.stdout
 
     if "Accepted" in result:
-        print("Cassius accepted the minimized version, backtracking...", file=sys.stderr)
-        return (True, False, [], -1)
+        return Backtrack()
     elif "Rejected" in result:
-        print("Cassius rejected the minimized version, continuing...", file=sys.stderr)
         lines = result.split()
         stats = json.loads(lines[1])
-        STATISTICS.append((stats, i))
-        return (False, False, lines[2:], stats["total"])
+        return Continue(stats, runtime, lines[2:])
     elif "Minimized" in result:
-        print("Minimized!", file=sys.stderr)
         lines = result.split()
         remaining_boxes = int(lines[1])
-        return (False, True, [], remaining_boxes)
+        return Done(remaining_boxes)
     elif "Error" in result:
-        print("Cassius encountered an error, terminating", file=sys.stderr)
-        raise Exception()
-
-    return (False, i)
+        raise Exception("Cassius encountered an error:\n" + result)
 
 def get_minimized(url, elts, name):
     prerun = "; ".join(["document.getElementsByTagName({}.tag)[{}.index].remove()".format(elt, elt) for elt in elts])
     get_bench(url, name=name, prerun=prerun)
 
 if __name__ == "__main__":
+    STATISTICS = []
+
     p = argparse.ArgumentParser(description="Download a website as Cassius test cases")
     p.add_argument("name", type=str, help="File name under bench/.")
     p.add_argument("urls", metavar="URLs", type=str, help="URLs to dowload")
@@ -56,24 +55,28 @@ if __name__ == "__main__":
     # Don't ever try to delete head, meta, title
     backtracked = [u'{"tag":"head","index":0}', u'{"tag":"meta","index":0}', u'{"tag":"title","index":0}']
     start = time.time()
-
-    get_minimized(args.urls, eliminated, args.name)
-    accepted, minimized, elts, initial = run_accept(args.name, args.name, backtracked, maxtime=args.timeout)
-    if accepted:
-        raise Exception("Full FWT accepted")
-    else:
-        iterations += 1
+    minimized = False
 
     while not minimized:
-        eliminated.extend(elts)
         name = "{}-{}-minimized".format(args.name, iterations)
+        print("Running Cassius:", file=sys.stderr)
         get_minimized(args.urls, eliminated, name)
-        accepted, minimized, elts, _ = run_accept(name, args.name, backtracked, maxtime=args.timeout)
-
-        if accepted:
+        res = run_accept(name, args.name, backtracked, maxtime=args.timeout)
+        minimized = instanceof(res, Done)
+        if instanceof(res, Continue):
+            print("Cassius rejected the minimized version, continuing...", file=sys.stderr)
+            if iterations == 0: initial = res.stats["total"]
+            eliminated.extend(res.elts)
+            STATISTICS.append((res.stats, res.runtime))
+        elif instanceof(res, Backtrack):
+            print("Cassius accepted the minimized version, backtracking...", file=sys.stderr)
+            if iterations == 0:
+                raise Exception("Full FWT accepted")
             backtracked.append(eliminated.pop())
             STATISTICS.pop()
-
+        elif instanceof(res, Done):
+            print("Minimized!", file=sys.stderr)
+            if iterations == 0: initial = res.size
         iterations += 1
 
     total_time = time.time() - start
