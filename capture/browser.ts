caf2618ed17d1394b10de47204ac28e4b9bebcb5
Pavel Panchekha <me@pavpanchekha.com>
2019-12-13 11:50:45 -0700

    Ah, no, this is what we want

--- capture/browser.ts
+++ capture/browser.ts
@@ -22,7 +22,7 @@ function default_font_size(family, features) {
     var div = document.createElement("CassiusBlock");
     body.appendChild(div);
     div.style.fontFamily = family;
-    div.style.fontSize = "1rem";
+    div.style.fontSize = "medium";
     div.style.fontWeight = "400";
     div.style.fontStyle = "normal";
     return val2px(cs(div, "font-size"), features);
