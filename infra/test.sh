b18b6cee0324e095031e7f8bede8dc2dc8e98fd8
Pavel Panchekha <me@pavpanchekha.com>
2019-12-18 14:57:16 -0700

    Reorder tests by version

--- infra/test.sh
+++ infra/test.sh
@@ -43,8 +43,9 @@ do
 done | xargs make -j$THREADS bench/fwt.rkt
 
 make FWT_PATH="$FWT_PATH" FLAGS="--verbose --threads $THREADS --cache reports/run.cache" \
-     reports/fwt.html reports/vizassert.html reports/csswg.html reports/specific.html \
-     reports/modular.html reports/bugs.html
+     reports/csswg.html reports/bugs.html \
+     reports/fwt.html reports/vizassert.html reports/specific.html \
+     reports/modular.html
 
 mkdir -p reports/rkt/
 mkdir -p reports/json/
