65f0bef1207ac1c8bd1bc49967c2a42dee3f7162
Pavel Panchekha <me@pavpanchekha.com>
2020-01-05 20:32:02 -0700

    Fixed the debugging help

--- src/report.rkt
+++ src/report.rkt
@@ -186,22 +186,22 @@
             (unless (null? to-send)
               (place-channel-put worker (cons worker (car to-send)))
               (set! to-send (cdr to-send))))
-          (with-handlers ([exn:break?
-                           (λ (e)
-                             (eprintf "Terminating after ~a/~a. Waiting for ~a\n"
-                                      (length out) (length inputs)
-                                      (car to-send))
-                             (raise e))])
             (let loop ([out '()])
-              (cond
-               [(= (length out) (length inputs))
-                (map cdr (sort out < #:key car))]
-               [else
-                (match-define (cons worker result) (apply sync workers))
-                (unless (null? to-send)
-                  (place-channel-put worker (cons worker (car to-send)))
-                  (set! to-send (cdr to-send)))
-                (loop (cons result out))]))))
+              (with-handlers ([exn:break?
+                               (λ (e)
+                                 (eprintf "Terminating after ~a/~a. Waiting for #~a\n"
+                                          (length out) (length inputs)
+                                          (first (set-subtract (range 0 (length inputs)) (set-union (map car to-send) (map car out)))))
+                                 (raise e))])
+                (cond
+                 [(= (length out) (length inputs))
+                  (map cdr (sort out < #:key car))]
+                 [else
+                  (match-define (cons worker result) (apply sync workers))
+                  (unless (null? to-send)
+                    (place-channel-put worker (cons worker (car to-send)))
+                    (set! to-send (cdr to-send)))
+                  (loop (cons result out))]))))
         (for/list ([input inputs]) body ...))))
 
 (define (run-regression-tests probs #:valid [valid? (const true)] #:index [index (hash)]
