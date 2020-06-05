d5c2c5d4caa7f37d90a2dc36abc6f45d3c8794d3
Skyler Griffith <skyler.griffith@utah.edu>
2020-06-05 15:44:38 -0600

    Add website for first test of java script to bench

---
+++ bench/js/button_test_1/one_press.js
@@ -0,0 +1,9 @@
+function add_element(){
+  	var list = document.getElementById("list");
+  	let list_element = document.createElement('li');
+  	list.appendChild(list_element);
+  	list_element.id = 'element-' + list.getElementsByTagName("li").length;
+	list_element.innerHTML = "value-" + list.getElementsByTagName("li").length;
+}
+
+add_element();
---
+++ bench/js/button_test_1/three_press.js
@@ -0,0 +1,11 @@
+function add_element(){
+	  	var list = document.getElementById("list");
+	  	let list_element = document.createElement('li');
+	  	list.appendChild(list_element);
+	  	list_element.id = 'element-' + list.getElementsByTagName("li").length;
+		list_element.innerHTML = "value-" + list.getElementsByTagName("li").length;
+}
+
+add_element();
+add_element();
+add_element();
