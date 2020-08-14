500992775a2e646139fc24ac9631c9560f09234c
Skyler Griffith <skyler.griffith@utah.edu>
2020-08-14 13:02:11 -0600

    Add a simple implementation of the constraints function needed to do a proof by induction

--- src/main.rkt
+++ src/main.rkt
@@ -115,6 +115,23 @@
 
     (emit `(assert (! ,constraint :named ,(sformat "~a/~a" fun (node-id elt)))))))
 
+;Sets up the special constraints needed to do a proof by induction on a given list
+(define (inductive-list-constraints dom emit box)
+  ;Note the header and footer of the list
+  (define header (node-fchild box))
+  (define footer (node-lchild box))
+  ;Note the box after the header and before the footer
+  (define leftbox (node-next header))
+  (define rightbox (node-prev footer))
+  ;Explicitely say what the neighbors and parent of the header are
+  (emit `(assert (and (= (pbox (dump-box header)) (dump-box box)) (= (vbox (dump-box header)) 0) (= (nbox (dump-box header)) (dump-box leftbox)))))
+  ;Explicitly state what the neighbors and parent of the footer are
+  (emit `(assert (and (= (pbox (dump-box footer)) (dump-box box)) (= (vbox (dump-box footer)) (dump-box rightbox)) (= (nbox (dump-box footer)) 0))))
+  ;Explicity state leftbox's neighbors and parent, leaving its next up to z3
+  (emit `(assert (and (= (pbox (dump-box leftbox)) (dump-box box)) (= (vbox (dump-box leftbox)) (dump-box header)))))
+  ;Explicitely state rightbox's neighbors and parent, leaving its previous up to z3
+  (emit `(addert (and (= (pbox (dump-box rightbox)) (dump-box box)) (= (nbox (dump-box rightbox)) (dump-box footer))))))
+
 (define (box-tree-constraints dom emit box)
   (define link-function
     (if (dom-context dom ':component)
